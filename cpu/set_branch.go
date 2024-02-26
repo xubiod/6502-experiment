220097ca6b5628c8edf2859ec311d376eadb45dc
Xubiod <xubiod.mail@gmail.com>
2024-02-26 09:04:06 -0500

    duplicate code reduction
    lol lmao

--- cpu/set_branch.go
+++ cpu/set_branch.go
@@ -1,105 +1,73 @@
 package cpu
 
+func makeSigned(i uint8) (o int8) {
+	o = int8(i & 0x7F)
+	if i&0b10000000 > 0 {
+		o *= -1
+	}
+	return
+}
+
 // Branch on Carry Clear
 func (c *Core) BCC__rel(raw uint8) {
-	var relative int8 = int8(raw & 0x7F)
-	if raw&0b10000000 > 0 {
-		relative *= -1
-	}
-
 	c.PC += 2
 	if c.Flags&FLAG_CARRY == 0 {
-		c.PC += uint16(relative)
+		c.PC += uint16(makeSigned(raw))
 	}
 }
 
 // Branch on Carry Set
 func (c *Core) BCS__rel(raw uint8) {
-	var relative int8 = int8(raw & 0x7F)
-	if raw&0b10000000 > 0 {
-		relative *= -1
-	}
-
 	c.PC += 2
 	if c.Flags&FLAG_CARRY != 0 {
-		c.PC += uint16(relative)
+		c.PC += uint16(makeSigned(raw))
 	}
 }
 
 // Branch on Result Not Zero
 func (c *Core) BNE__rel(raw uint8) {
-	var relative int8 = int8(raw & 0x7F)
-	if raw&0b10000000 > 0 {
-		relative *= -1
-	}
-
 	c.PC += 2
 	if c.Flags&FLAG_ZERO == 0 {
-		c.PC += uint16(relative)
+		c.PC += uint16(makeSigned(raw))
 	}
 }
 
 // Branch on Result Zero
 func (c *Core) BEQ__rel(raw uint8) {
-	var relative int8 = int8(raw & 0x7F)
-	if raw&0b10000000 > 0 {
-		relative *= -1
-	}
-
 	c.PC += 2
 	if c.Flags&FLAG_ZERO != 0 {
-		c.PC += uint16(relative)
+		c.PC += uint16(makeSigned(raw))
 	}
 }
 
 // Branch on Result Plus
 func (c *Core) BPL__rel(raw uint8) {
-	var relative int8 = int8(raw & 0x7F)
-	if raw&0b10000000 > 0 {
-		relative *= -1
-	}
-
 	c.PC += 2
 	if c.Flags&FLAG_NEGATIVE == 0 {
-		c.PC += uint16(relative)
+		c.PC += uint16(makeSigned(raw))
 	}
 }
 
 // Branch on Result Minus
 func (c *Core) BMI__rel(raw uint8) {
-	var relative int8 = int8(raw & 0x7F)
-	if raw&0b10000000 > 0 {
-		relative *= -1
-	}
-
 	c.PC += 2
 	if c.Flags&FLAG_NEGATIVE != 0 {
-		c.PC += uint16(relative)
+		c.PC += uint16(makeSigned(raw))
 	}
 }
 
 // Branch on Overflow Clear
 func (c *Core) BVC__rel(raw uint8) {
-	var relative int8 = int8(raw & 0x7F)
-	if raw&0b10000000 > 0 {
-		relative *= -1
-	}
-
 	c.PC += 2
 	if c.Flags&FLAG_OVERFLOW == 0 {
-		c.PC += uint16(relative)
+		c.PC += uint16(makeSigned(raw))
 	}
 }
 
 // Branch on Overflow Set
 func (c *Core) BVS__rel(raw uint8) {
-	var relative int8 = int8(raw & 0x7F)
-	if raw&0b10000000 > 0 {
-		relative *= -1
-	}
-
 	c.PC += 2
 	if c.Flags&FLAG_OVERFLOW != 0 {
-		c.PC += uint16(relative)
+		c.PC += uint16(makeSigned(raw))
 	}
 }
