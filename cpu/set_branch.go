b99768cbdca943d8c54a47a0b40e7d66139062d9
Xubiod <xubiod.mail@gmail.com>
2024-02-26 09:07:27 -0500

    wagabababobo

--- cpu/set_branch.go
+++ cpu/set_branch.go
@@ -1,18 +1,18 @@
 package cpu
 
-func makeSigned(i uint8) (o int8) {
-	o = int8(i & 0x7F)
+func branchVal(i uint8) (o uint16) {
+	m := int8(i & 0x7F)
 	if i&0b10000000 > 0 {
-		o *= -1
+		m *= -1
 	}
-	return
+	return uint16(m)
 }
 
 // Branch on Carry Clear
 func (c *Core) BCC__rel(raw uint8) {
 	c.PC += 2
 	if c.Flags&FLAG_CARRY == 0 {
-		c.PC += uint16(makeSigned(raw))
+		c.PC += branchVal(raw)
 	}
 }
 
@@ -20,7 +20,7 @@ func (c *Core) BCC__rel(raw uint8) {
 func (c *Core) BCS__rel(raw uint8) {
 	c.PC += 2
 	if c.Flags&FLAG_CARRY != 0 {
-		c.PC += uint16(makeSigned(raw))
+		c.PC += branchVal(raw)
 	}
 }
 
@@ -28,7 +28,7 @@ func (c *Core) BCS__rel(raw uint8) {
 func (c *Core) BNE__rel(raw uint8) {
 	c.PC += 2
 	if c.Flags&FLAG_ZERO == 0 {
-		c.PC += uint16(makeSigned(raw))
+		c.PC += branchVal(raw)
 	}
 }
 
@@ -36,7 +36,7 @@ func (c *Core) BNE__rel(raw uint8) {
 func (c *Core) BEQ__rel(raw uint8) {
 	c.PC += 2
 	if c.Flags&FLAG_ZERO != 0 {
-		c.PC += uint16(makeSigned(raw))
+		c.PC += branchVal(raw)
 	}
 }
 
@@ -44,7 +44,7 @@ func (c *Core) BEQ__rel(raw uint8) {
 func (c *Core) BPL__rel(raw uint8) {
 	c.PC += 2
 	if c.Flags&FLAG_NEGATIVE == 0 {
-		c.PC += uint16(makeSigned(raw))
+		c.PC += branchVal(raw)
 	}
 }
 
@@ -52,7 +52,7 @@ func (c *Core) BPL__rel(raw uint8) {
 func (c *Core) BMI__rel(raw uint8) {
 	c.PC += 2
 	if c.Flags&FLAG_NEGATIVE != 0 {
-		c.PC += uint16(makeSigned(raw))
+		c.PC += branchVal(raw)
 	}
 }
 
@@ -60,7 +60,7 @@ func (c *Core) BMI__rel(raw uint8) {
 func (c *Core) BVC__rel(raw uint8) {
 	c.PC += 2
 	if c.Flags&FLAG_OVERFLOW == 0 {
-		c.PC += uint16(makeSigned(raw))
+		c.PC += branchVal(raw)
 	}
 }
 
@@ -68,6 +68,6 @@ func (c *Core) BVC__rel(raw uint8) {
 func (c *Core) BVS__rel(raw uint8) {
 	c.PC += 2
 	if c.Flags&FLAG_OVERFLOW != 0 {
-		c.PC += uint16(makeSigned(raw))
+		c.PC += branchVal(raw)
 	}
 }
